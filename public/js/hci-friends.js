66e384b343b3e7cfec2f557de00833c4f0f74b52
Nicholas Schenone <nschenone16@gmail.com>
2020-02-06 14:57:21 -0800

    Did lab

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Call this function when the page loads (the "ready" event)
-$(document).ready(function() {
+$(document).ready(function () {
 	initializePage();
 })
 
@@ -12,12 +12,19 @@ function initializePage() {
 	console.log("Javascript connected!");
 }
 
+$(".friend-name").click(clickedFriend);
+
+function clickedFriend(e) {
+	e.preventDefault();
+	$(this).text(anagrammedName($(this).text()))
+}
+
 function anagrammedName(name) {
 	// Thanks, Internet Anagram Server!
-	
+
 	if (name == "Doug Engelbart") {
 		return "Notable Grudge";
-	} 
+	}
 	else if (name == "Ivan Sutherland") {
 		return "Vandal Heist Run";
 	}
--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,13 @@
 var data = require("../data.json");
 
-exports.addFriend = function(request, response) {    
-	// Your code goes here
- }
+exports.addFriend = function (request, response) {
+	var new_friend = {
+		"name": request.query.name,
+		"description": request.query.description,
+		"imageURL": "http://lorempixel.com/400/400/people/"
+	}
+
+	data.friends.push(new_friend);
+
+	response.render('index', data);
+}
